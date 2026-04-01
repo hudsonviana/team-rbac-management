3192e996399daab2c2e63505c683ba250674f236
Hudson Andrade Viana <hudson1206@gmail.com>
2026-04-01 13:03:47 -0300

    Implement & Test Register User API

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,18 +2,23 @@ import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
 
 export async function checkDatabaseConnection(): Promise<boolean> {
   try {
-    await prisma.$queryRaw`Select 1`;
+    await prisma.$queryRaw`SELECT 1`;
     return true;
   } catch (error) {
     console.error(`Falha ao conectar ao banco de dados: ${error}`);
     return false;
   }
 }
-
-export { prisma };
